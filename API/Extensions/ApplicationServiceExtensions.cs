9aed70ac5ae2f9c8424711747c87dda2502e61a0
Nisha <nisha.karnire@gmail.com>
2021-07-11 01:55:43 -0400

    End of Section 14

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -19,6 +19,7 @@ namespace API.Extensions
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddScoped<LogUserActivity>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddDbContext<DataContext>(options=>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
